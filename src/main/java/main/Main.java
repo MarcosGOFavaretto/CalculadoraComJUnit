555eef128f1931be7eb1e5388ba8f53a06380d06
MarcosGOFavaretto <marcosfavaretto.dev@gmail.com>
2021-12-25 08:45:32 -0300

    :rocket:FEATURE: Adiciona a funcionalidade de inserção de valores pelo usuário e trata mensagem de retorno

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,15 +1,27 @@
 package main;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int num1 = 8;
-		int num2 = 2;
 
+		Scanner scanner = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		System.out.print("Insira o primeiro número:");
+		int num1 = scanner.nextInt();
+		System.out.print("Insira o segundo número:");
+		int num2 = scanner.nextInt();
+
+		System.out.println("\nRESULTADO");
 		System.out.println("O valor da soma é: " + somar(num1, num2));
 		System.out.println("O valor da subtração é: " + subtrair(num1, num2));
 		System.out.println("O valor da multiplicação é: " + multiplicar(num1, num2));
 		System.out.println("O valor da divisão é: " + dividir(num1, num2));
+
+		scanner.close();
 	}
 
 	public static int somar(int num1, int num2) {
